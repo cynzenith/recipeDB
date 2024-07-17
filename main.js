62ccafa422dc477f9af9fd570d67144f918e9ef1
cynzenith~ <cynzenith@gmail.com>
2024-07-17 10:36:38 +0900

    second commit

--- main.js
+++ main.js
@@ -2,22 +2,20 @@ const API_KEY = `7109179c29414b97985b`;
 const serviceId = 'COOKRCP01';
 const dataType = 'json';
 let startIdx = '1';
-let endIdx = '20';
+let endIdx = '10';
 
 let recipes = []
 
 // 전체 레시피 url
-let url_object = new URL(`https://charming-cactus-400740.netlify.app/api/${API_KEY}/${serviceId}/${dataType}/${startIdx}/${endIdx}`);
+let url_object = new URL(`https://charming-cactus-400740.netlify.app/api/${API_KEY}/${serviceId}/${dataType}/${startIdx}/${endIdx}/RCP_NM=가`);
 
-const RecipeTypes = document.querySelectorAll('.RecipeTypes button');
-// console.log(RecipeTypes)
-
-const Ingredients = document.querySelectorAll('.Ingredients button');
-// console.log(ingredients)
+const RecipeTypes = document.querySelectorAll('.RecipeTypes button'); // console.log(RecipeTypes)
+const Ingredients = document.querySelectorAll('.Ingredients button'); // console.log(ingredients)
 
 RecipeTypes.forEach(RecipeType=> RecipeType.addEventListener("click",(event)=>getRecipeByRecipeType(event)))
 Ingredients.forEach(Ingredient=> Ingredient.addEventListener("click",(event)=>getRecipeByIngredient(event)))
 
+// 카테고리 검색(1) : 요리 종류 ['반찬', '국&찌개', '후식', '일품', '밥', '기타']
 const getRecipeByRecipeType = async(event) => {
     const RecipeType = event.target.textContent
     const encodedRecipeType = RecipeType.toString();
@@ -26,6 +24,7 @@ const getRecipeByRecipeType = async(event) => {
     await getRecipes()
 }
 
+// 카테고리 검색(2) : 요리 재료 ['양파', '물', '설탕', '소금', '참기름', '식초', '당근', '마늘', '간장', '홍고추', '통깨', '후추']
 const getRecipeByIngredient = async(event) => {
     const Ingredient = event.target.textContent
     console.log(Ingredient)
@@ -33,11 +32,18 @@ const getRecipeByIngredient = async(event) => {
     await getRecipes()
 }
 
-// 전체 레시피 데이터를 가져오는 함수
+// [검색]을 통해 특정 메뉴의 레시피 데이터를 가져오는 함수
+const getRecipeByKeyword = async() => {
+    const recipeName = document.getElementById('search-input').value
+    url_object = new URL(`https://charming-cactus-400740.netlify.app/api/${API_KEY}/${serviceId}/${dataType}/${startIdx}/${endIdx}/RCP_NM=${recipeName}`);
+    await getRecipes()
+}
+
+// url를 바탕으로 레시피 데이터를 가져오는 함수
 const getRecipes = async() => {
     const response = await fetch(url_object)
     const data = await response.json()
-    console.log("data 결과 : ", data)
+    // console.log("data 결과 : ", data)
 
     total_count = data.COOKRCP01.total_count
     console.log("total_count 결과 : ", total_count)
@@ -48,14 +54,7 @@ const getRecipes = async() => {
     render()
 }
 
-
-// 특정 레시피 데이터를 가져오는 함수
-let recipeName = '된장국'
-const getRecipesName = async() => {
-    url_object = new URL(`https://charming-cactus-400740.netlify.app/api/${API_KEY}/${serviceId}/${dataType}/${startIdx}/${endIdx}/RCP_NM=${recipeName}`);
-    await getRecipes()
-}
-
+// 레시피 데이터를 바탕으로 HTML를 작성하여 화면에 출력하는 함수
 const render = () => {
     // recipes 배열에 있는 요소를 반복하여 recipeHTML에 대입
     const recipeHTML = recipes.map(item => {
@@ -94,6 +93,20 @@ const render = () => {
 
 
 
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display === "inline") {
+        inputArea.style.display = "none";
+    } else {
+        inputArea.style.display = "inline";
+    }
+};
+
+function handleKeyDown(event) {
+    if (event.key === "Enter") {
+        getRecipeByKeyword();
+    }
+}
 
 getRecipes()
 // getRecipesName()
